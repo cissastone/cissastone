b0c0fa84e5173aff5456ca96fb355dccfecbc7e2
MehmetBardakci <m.bardakciw@gmail.com>
2024-10-27 17:50:49 +0300

    sadsa

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Products from "./_components/Products";
 import Image from "next/image";
 
+export const revalidate = 0;
 export default function Home() {
   return (
     <main>
