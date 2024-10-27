b29f79978b2dd7b9c21317adc770f1f7032a79e0
MehmetBardakci <m.bardakciw@gmail.com>
2024-10-27 22:27:27 +0300

    revalidate delete özelliği

--- lib/actions/deleteProduct.ts
+++ lib/actions/deleteProduct.ts
@@ -1,6 +1,8 @@
+'use server'
 import { deleteDoc, doc, getDoc } from "firebase/firestore";
 import { initializeFirebaseClient } from "../FirebaseClient/config";
 import { deleteObject, ref } from "firebase/storage";
+import { revalidatePath } from "next/cache";
 
 export async function deleteProductData(productid:string){
     const {db,storage}=initializeFirebaseClient();
@@ -19,6 +21,8 @@ export async function deleteProductData(productid:string){
        
         await deleteDoc(collectionRef);
         console.log("Ürün başarıyla silindi!");
+        revalidatePath("/adminDashboard")
+    revalidatePath("/")
 
     } catch (error) {
         console.error("Hata:", error);
